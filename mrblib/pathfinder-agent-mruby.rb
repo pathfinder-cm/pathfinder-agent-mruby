729aaabf79b6f51dc484ee0744463278101c6930
Giovanni Sakti <giosakti@gmail.com>
2019-04-17 18:43:59 +0700

    share token by file between 2 running process

--- mrblib/pathfinder-agent-mruby.rb
+++ mrblib/pathfinder-agent-mruby.rb
@@ -42,30 +42,10 @@ Contains node agent and metrics agent.
       puts "Node agent starting..."
       puts "\n"
 
-      puts "Active configurations:"
-      CONFIG.each do |k,v|
-        puts "#{k}: #{v}"
-      end
-      puts "\n"
-
-      while true do
-        ok, _ = pathfinder.register(
-          cluster_password: CONFIG[:PF_CLUSTER_PASSWORD],
-          node: node
-        )
-
-        if ok
-          puts "Agent successfully registered!"
-          break
-        else
-          retry_wait = 60 + (1..10).to_a
-          puts "Registration failure, retrying in #{retry_wait} seconds"
-          sleep retry_wait
-        end
-      end
-
-      agent = PathfinderAgentMruby::NodeAgent.new(pathfinder: pathfinder, lxd: lxd)
-      agent.run
+      print_configuration
+      pathfinder = register_agent(pathfinder: pathfinder, node: node)
+      node_agent = PathfinderAgentMruby::NodeAgent.new(pathfinder: pathfinder, lxd: lxd)
+      node_agent.run
     }
   )
   start_command.add_command(start_node_command)
@@ -78,28 +58,8 @@ Contains node agent and metrics agent.
       puts "Metrics agent starting..."
       puts "\n"
 
-      puts "Active configurations:"
-      CONFIG.each do |k,v|
-        puts "#{k}: #{v}"
-      end
-      puts "\n"
-
-      while true do
-        ok, _ = pathfinder.register(
-          cluster_password: CONFIG[:PF_CLUSTER_PASSWORD],
-          node: node
-        )
-
-        if ok
-          puts "Agent successfully registered!"
-          break
-        else
-          retry_wait = 60 + (1..10).to_a
-          puts "Registration failure, retrying in #{retry_wait} seconds"
-          sleep retry_wait
-        end
-      end
-
+      print_configuration
+      pathfinder = register_agent(pathfinder: pathfinder, node: node)
       metrics_agent = PathfinderAgentMruby::MetricsAgent.new(pathfinder: pathfinder)
       metrics_agent.run
     }
@@ -107,4 +67,51 @@ Contains node agent and metrics agent.
   start_command.add_command(start_metrics_command)
 
   root_command.execute(argv)
+end
+
+def print_configuration
+  puts "Active configurations:"
+  CONFIG.each do |k,v|
+    puts "#{k}: #{v}"
+  end
+  puts "\n"
+end
+
+def register_agent(pathfinder:, node:)
+  authentication_token = nil
+  if File.exist?("#{ENV['HOME']}/.pathfinder/node-token")
+    File.foreach("#{ENV['HOME']}/.pathfinder/node-token") do |line|
+      authentication_token = line
+    end
+  end
+
+  if authentication_token.nil? || authentication_token == ''
+    while true do
+      ok, authentication_token = pathfinder.register(
+        cluster_password: CONFIG[:PF_CLUSTER_PASSWORD],
+        node: node
+      )
+
+      if ok
+        puts "Agent successfully registered!"
+        Dir.mkdir("#{ENV['HOME']}/.pathfinder")
+        File.open("#{ENV['HOME']}/.pathfinder/node-token", 'w') do |f|
+          f.write authentication_token
+        end
+        break
+      else
+        retry_wait = 60 + (1..10).to_a
+        puts "Registration failure, retrying in #{retry_wait} seconds"
+        sleep retry_wait
+      end
+    end
+  end
+
+  Pathfinder::PathfinderNode.new(
+    cluster_name: CONFIG[:PF_CLUSTER],
+    authentication_token: authentication_token,
+    scheme: CONFIG[:PF_SERVER_SCHEME],
+    address: CONFIG[:PF_SERVER_ADDR],
+    port: CONFIG[:PF_SERVER_PORT]
+  )
 end
